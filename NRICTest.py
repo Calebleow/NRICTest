db6808bb1f439391354110337c443fbf2c4ea5fa
Calebleow <84079022+Calebleow@users.noreply.github.com>
2021-07-02 22:07:58 +0800

    v1.11

--- NRICTest.py
+++ NRICTest.py
@@ -1,10 +1,31 @@
-NRICValidation = x
+def generate_last_letter(nric):
+    prefix = nric[0].upper()
+    # suffix = nric[-1].upper()
+    number_string = nric[1:]
+    numbers = []
 
-fg_map = ["X", "W", "U", "T", "R", "Q", "P", "N", "M", "L","K"]
-st_map = ["J", "Z", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+    fg_map = ['X', 'W', 'U', 'T', 'R', 'Q', 'P', 'N', 'M', 'L', 'K']
+    st_map = ['J', 'Z', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A']
 
-weights = [2, 7, 6, 5, 4, 3, 2]
+    for n in number_string:
+        numbers.append(int(n))
+    weights = [2, 7, 6, 5, 4, 3, 2]
 
-sum = enumerate
+    number_sum = 0
+    for w, n in zip(weights, numbers):
+        number_sum += n * w
+    
+    if prefix == 'T' or prefix == 'G':
+        number_sum += 4
 
-remainder = sum%11
+    remainder = number_sum % 11
+
+    if prefix == 'F' or prefix == 'G':
+        return fg_map[remainder]
+
+    if prefix == 'S' or prefix == 'T':
+        return st_map[remainder]
+
+nric = "S9045061"
+
+print(generate_last_letter(nric))
--- Responses.py
+++ Responses.py
@@ -1,7 +1,39 @@
 def responses(input_text):
-    user_message = str(input_text).upper()
+    user_message = str(input_text)
 
     if user_message in ("NRIC"):
-        return ("Correct suffix")
+        return (generate_last_letter(nric))
     else : 
         return ("Try again mortal")
+
+def generate_last_letter(nric):
+    prefix = nric[0].upper()
+    # suffix = nric[-1].upper()
+    number_string = nric[1:]
+    numbers = []
+
+    fg_map = ['X', 'W', 'U', 'T', 'R', 'Q', 'P', 'N', 'M', 'L', 'K']
+    st_map = ['J', 'Z', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A']
+
+    for n in number_string:
+        numbers.append(int(n))
+    weights = [2, 7, 6, 5, 4, 3, 2]
+
+    number_sum = 0
+    for w, n in zip(weights, numbers):
+        number_sum += n * w
+    
+    if prefix == 'T' or prefix == 'G':
+        number_sum += 4
+
+    remainder = number_sum % 11
+
+    if prefix == 'F' or prefix == 'G':
+        return fg_map[remainder]
+
+    if prefix == 'S' or prefix == 'T':
+        return st_map[remainder]
+
+nric = "S9045061"
+
+print(generate_last_letter(nric))
