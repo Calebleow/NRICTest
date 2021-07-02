86566b601f8fd5f0dc8f7c1db4a0e07863185186
Calebleow <84079022+Calebleow@users.noreply.github.com>
2021-07-02 23:38:13 +0800

    v1.a Branch on Browser method

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import Constants as keys
 from telegram.ext import *
 import Responses as R
+import json 
+import requests
 
 print("Botted")
 
-def start_command(update, context):
-    update.message.reply_text('lets go')
+TOKEN = keys
+URL = "https://api.telegram.org/bot{}/".format(TOKEN.API_KEY)
 
-def help_command(update, context):
-    update.message.reply_text('Type the NRIC you wanna check without the last letter')
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
     
-def handle_message(update, context):
-    text = str(update.message.text).upper()
-    response = R.responses(text)
 
-    update.message.reply_text(response)
-
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
-
-def main():
-    updater = Updater(keys.API_KEY, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
-
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
+text, chat = get_last_chat_id_and_text(get_updates())
+send_message(text, chat)
 
 if __name__ == '__main__':
     main()
